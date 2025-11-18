f696868f61f180ceb5cafbe206738b54dd4da6d8
Sebastian C <sebsebmc@gmail.com>
2025-11-18 15:10:02 -0600

    Refactor messages and add APIVersions basics

--- app/main.go
+++ app/main.go
@@ -8,65 +8,6 @@ import (
 	"time"
 )
 
-const UNSUPPORTED_VERSION = 35
-
-type Message struct {
-	message_size int32
-}
-
-type Response struct {
-	Message
-	correlation_id int32
-	body           []byte
-}
-
-type Request struct {
-	Message
-	request_api_key     int16
-	request_api_version int16
-	correlation_id      int32
-	client_id           string
-	body                []byte
-}
-
-func (m Message) AppendBinary(b []byte) ([]byte, error) {
-	bytes, err := m.MarshalBinary()
-	if err != nil {
-		return nil, err
-	}
-	return append(b, bytes...), nil
-}
-
-func (m Message) MarshalBinary() (data []byte, err error) {
-	data = make([]byte, 0)
-	data = binary.BigEndian.AppendUint32(data, uint32(m.message_size))
-	return
-}
-
-func (m *Message) UnmarshalBinary(in []byte) error {
-	m.message_size = int32(binary.BigEndian.Uint32(in))
-	return nil
-}
-
-func (r *Request) UnmarshalBinary(in []byte) error {
-	r.Message.UnmarshalBinary(in)
-	r.request_api_key = int16(binary.BigEndian.Uint16(in[4:]))
-	r.request_api_version = int16(binary.BigEndian.Uint16(in[6:]))
-	r.correlation_id = int32(binary.BigEndian.Uint32(in[8:]))
-
-	return nil
-}
-
-func (r Response) MarshalBinary() ([]byte, error) {
-	out, err := r.Message.MarshalBinary()
-	if err != nil {
-		return nil, err
-	}
-	out = binary.BigEndian.AppendUint32(out, uint32(r.correlation_id))
-	out = append(out, r.body...)
-	return out, nil
-}
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -91,14 +32,27 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	requestBytes := make([]byte, 12)
-	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	conn.SetReadDeadline(time.Now().Add(10 * time.Millisecond))
+	msg_size := make([]byte, 4)
+	conn.Read(msg_size)
+	requestBytes := append(make([]byte, binary.BigEndian.Uint32(msg_size)+4), msg_size...)
 	conn.Read(requestBytes)
 	r := new(Request)
 	r.UnmarshalBinary(requestBytes)
-	resp := Response{Message{0}, r.correlation_id, []byte{}}
-	if r.request_api_version > 4 {
-		resp.body = binary.BigEndian.AppendUint16(resp.body, 35)
+	// fmt.Printf("Requested API %d with version %d\n", r.request_api_key, r.request_api_version)
+	resp := Message{message_size: 0, header: &ResponseHeader{r.correlation_id}}
+	if r.request_api_key == API_KEY_APIVERSIONS {
+		if r.request_api_version > 4 || r.request_api_key < 0 {
+			var rbody ResponseBody
+			rbody.body = make([]byte, 0)
+			rbody.body = binary.BigEndian.AppendUint16(rbody.body, 35)
+			resp.body = &rbody
+		} else {
+			var rbody ApiVersionsV4ResponseBody
+			rbody.api_keys = []ApiKeys{{api_key: API_KEY_APIVERSIONS, min_version: 0, max_version: 4}}
+			// fmt.Println("Sending APIVersions response")
+			resp.body = &rbody
+		}
 	}
 
 	respBytes, _ := resp.MarshalBinary()
---
+++ app/protocol.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"encoding"
+	"encoding/binary"
+	"fmt"
+)
+
+const UNSUPPORTED_VERSION = 35
+
+const API_KEY_APIVERSIONS = 18
+
+type TaggedBuffer struct {
+	tags []string
+}
+
+func (t TaggedBuffer) MarshalBinary() ([]byte, error) {
+	if len(t.tags) == 0 {
+		return []byte{0}, nil
+	}
+	return nil, fmt.Errorf("Non-Empty TaggedBuffer not implemented")
+}
+
+func (t TaggedBuffer) AppendBinary(in []byte) ([]byte, error) {
+	if len(t.tags) == 0 {
+		in = append(in, 0)
+		return in, nil
+	}
+	return nil, fmt.Errorf("Non-Empty TaggedBuffer not implemented")
+}
+
+type RequestOrResponse interface {
+	encoding.BinaryAppender
+	encoding.BinaryUnmarshaler
+}
+
+type Message struct {
+	message_size int32
+	header       RequestOrResponse
+	body         RequestOrResponse
+}
+
+func (m Message) MarshalBinary() (data []byte, err error) {
+	data = make([]byte, 4)
+	data, err = m.header.AppendBinary(data)
+	if err != nil {
+		return nil, err
+	}
+	data, err = m.body.AppendBinary(data)
+	if err != nil {
+		return nil, err
+	}
+	m.message_size = int32(len(data) - 4)
+	binary.BigEndian.PutUint32(data, uint32(m.message_size))
+	return data, nil
+}
+
+func (m *Message) UnmarshalBinary(in []byte) error {
+	m.message_size = int32(binary.BigEndian.Uint32(in))
+	return nil
+}
+
+type ResponseHeader struct {
+	correlation_id int32
+}
+
+func (r ResponseHeader) AppendBinary(in []byte) ([]byte, error) {
+	in = binary.BigEndian.AppendUint32(in, uint32(r.correlation_id))
+	return in, nil
+}
+
+func (r *ResponseHeader) UnmarshalBinary(in []byte) error {
+	r.correlation_id = int32(binary.BigEndian.Uint32(in))
+	return nil
+}
+
+type ResponseBody struct {
+	body []byte
+}
+
+func (r ResponseBody) AppendBinary(in []byte) ([]byte, error) {
+	in = append(in, r.body...)
+	return in, nil
+}
+
+func (r *ResponseBody) UnmarshalBinary(in []byte) error {
+	r.body = in
+	return nil
+}
+
+type Request struct {
+	request_api_key     int16
+	request_api_version int16
+	correlation_id      int32
+	client_id           string
+}
+
+func (r *Request) UnmarshalBinary(in []byte) error {
+	r.request_api_key = int16(binary.BigEndian.Uint16(in))
+	r.request_api_version = int16(binary.BigEndian.Uint16(in[2:]))
+	r.correlation_id = int32(binary.BigEndian.Uint32(in[4:]))
+
+	return nil
+}
+
+type ApiVersionsV4ResponseBody struct {
+	error_code       int16
+	api_keys         []ApiKeys
+	throttle_time_ms int32
+	tag_buffer       TaggedBuffer
+}
+
+func (rb ApiVersionsV4ResponseBody) AppendBinary(in []byte) ([]byte, error) {
+	in = binary.BigEndian.AppendUint16(in, uint16(rb.error_code))
+	var err error
+	in = binary.AppendVarint(in, int64(len(rb.api_keys)))
+	for i := range rb.api_keys {
+		in, err = rb.api_keys[i].AppendBinary(in)
+		if err != nil {
+			return nil, err
+		}
+	}
+	in = binary.BigEndian.AppendUint32(in, uint32(rb.throttle_time_ms))
+	in, err = rb.tag_buffer.AppendBinary(in)
+	if err != nil {
+		return nil, err
+	}
+	return in, nil
+}
+
+func (rb *ApiVersionsV4ResponseBody) UnmarshalBinary(in []byte) error {
+	return fmt.Errorf("Unimplemented")
+}
+
+type ApiKeys struct {
+	api_key     int16
+	min_version int16
+	max_version int16
+	tag_buffer  TaggedBuffer
+}
+
+func (k ApiKeys) AppendBinary(in []byte) ([]byte, error) {
+	in = binary.BigEndian.AppendUint16(in, uint16(k.api_key))
+	in = binary.BigEndian.AppendUint16(in, uint16(k.min_version))
+	in = binary.BigEndian.AppendUint16(in, uint16(k.max_version))
+	in, err := k.tag_buffer.AppendBinary(in)
+	if err != nil {
+		return nil, err
+	}
+	return in, nil
+}
