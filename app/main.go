38c68c63ba2074a9f1e65d800993042034c7166a
Sebastian C <sebsebmc@gmail.com>
2025-11-17 19:33:11 -0600

    Add connection loop

--- app/main.go
+++ app/main.go
@@ -24,23 +24,29 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// TODO: Uncomment the code below to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
 	request := make([]byte, 8)
 	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
 	conn.Read(request)
 	temp := Message{0, 7}
 	conn.Write(temp.ToBytes())
-
 }
