92fe81587b1131ba1c170cefde37b11381d083fd
Sebastian C <sebsebmc@gmail.com>
2025-11-17 19:26:41 -0600

    respond to request with dummy corr_id of 7

--- app/main.go
+++ app/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
+
+type Message struct {
+	message_size int
+	header       int
+}
+
+func (m Message) ToBytes() []byte {
+	out := make([]byte, 0)
+	out = binary.BigEndian.AppendUint32(out, uint32(m.message_size))
+	out = binary.BigEndian.AppendUint32(out, uint32(m.header))
+	return out
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -17,9 +31,16 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	request := make([]byte, 8)
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	conn.Read(request)
+	temp := Message{0, 7}
+	conn.Write(temp.ToBytes())
+
 }
